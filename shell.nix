90f8bcee2e170f61a3404a46fd46f10c9475f976
Nicholas Davies <nicholasjohndavies42@gmail.com>
2024-12-06 11:39:25 +0800

    nix shell

---
+++ shell.nix
@@ -0,0 +1,13 @@
+# This is the shell environment for the nix shell (if you don't know what that is do not worry, disregard this file)
+
+let
+  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/cf8cc1201be8bc71b7cbbbdaf349b22f4f99c7ae.tar.gz") {};
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+      # select Python packages here
+      pandas
+      requests
+    ]))
+  ];
+}
